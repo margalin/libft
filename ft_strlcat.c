2b6e959977686d449110dbe53ee17c615228a22e
margalin <margalinve@gmail.com>
2026-06-01 06:47:01 +0000

    prueba

---
+++ ft_strlcat.c
@@ -0,0 +1,28 @@
+#include "libtf.h"
+
+size_t ft_strlcat(char *dst, const char *src, size_t l){
+    size_t lsrc;
+    size_t ldst;
+    size_t i;
+
+    ldst = ft_strlen(dst);
+    lsrc = ft_strlen(src);
+    i = 0;
+
+    if (ldst == 0){return(lsrc);}
+    if (ldst <= lsrc){return(lsrc+=l;)}
+    else{lsrc += ldst;}
+    while(src[i] != '\0' && ldst < (l - 1) && dst != src){
+        dst[ldst] = src[i];
+        i++;
+        ldst++;
+    }
+    dst[ldst] = '\0';
+    return(lsrc);
+}
+
+// Añade el string terminado en nulo src a dst y devuelve el tamaño total
+// Tamaño que será src+dst, l = tamaño destino buffer
+// si el tamaño del dst=0, la longitud es la origen
+// si el dst es menor al origen, devuelvo el origen más la longitud declara por entrada
+// si no, se recorre src hasta el nulo, se comprueba que el tamaño del dst siga siendo 1 menor al de entrada, y el dst sea diferente al origen
--- libft.h
+++ libft.h
@@ -10,8 +10,13 @@ int ft_isdigit(int c);
 int ft_isalnum(int c);
 int ft_isascii(int c);
 int ft_isprint(int c);
-size_t ft_strlen(const char *c);
-void *ft_memset(void *c, int d, size_t l);
+size_t ft_strlen(const char *s);
+void *ft_memset(void *s, int d, size_t l);
+void *ft_bzero(void *s, size_t l);
+void *ft_memcpy(char *d, const char *s, size_t ld);
+void *ft_memmove(void *d, const void *s, size_t ls);
+void *ft_strlcpy(char *d, const char *s, size_t l)
+
 
 
 #endif
